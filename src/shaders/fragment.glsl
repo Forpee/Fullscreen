ee06f5bfc019eea5f4f576a17ee295192790e86c
Forpee <khalilhassam3@gmail.com>
2022-06-02 19:53:18 +0200

    u

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -9,7 +9,7 @@ varying vec2 vUv;
 
 void main()
 {
-    float normSpeed=clamp(speed*10.,0.,1.);
+    float normSpeed=clamp(speed*40.,0.,1.);
     float mouseDist=length(vUv-uMouse);
     
     float c=smoothstep(.2,0.,mouseDist);
@@ -17,11 +17,12 @@ void main()
     vec2 newUv=(vUv-vec2(.5))*uResolution.zw+vec2(.5);
     vec4 color=texture2D(uTexture,newUv);
     
-    float r=texture2D(uTexture,newUv+.5*c*normSpeed).r;
-    float g=texture2D(uTexture,newUv+.3*c*normSpeed).g;
-    float b=texture2D(uTexture,newUv+.1*c*normSpeed).b;
+    float r=texture2D(uTexture,newUv+.3*c*normSpeed).r;
+    float g=texture2D(uTexture,newUv+.2*c*normSpeed).g;
+    float b=texture2D(uTexture,newUv+.3*c*normSpeed).b;
     
     gl_FragColor=color;
     gl_FragColor=vec4(normSpeed*mouseDist,0.,0.,1.);
     gl_FragColor=vec4(c,0.,0.,1.);
+    gl_FragColor=vec4(r,g,b,1.);
 }
